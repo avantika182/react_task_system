fa6f42621fb6da2007065043bbfd6430b41d4015
Your Name <joshnabensid@gmail.com>
2026-02-02 12:14:01 +0530

    [AS][added]

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,66 +1,131 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"
-import "./Register.css"
+import { Link, useNavigate } from "react-router-dom";
+import "./Register.css";
+
 const Register = () => {
-  //Declaration Section
   const [formData, setFormData] = useState({
-    fullName: "",
+    name: "",
     email: "",
     password: "",
     phone: "",
   });
 
-  //Design Section
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate()
+
+  //  Validation
+  const validate = () => {
+    const newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = "Full name is required";
+    } else if (formData.name.length < 3) {
+      newErrors.name = "Minimum 3 characters required";
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!formData.phone.trim()) {
+      newErrors.phone = "Phone number is required";
+    } else if (!/^[0-9]{10}$/.test(formData.phone)) {
+      newErrors.phone = "Phone must be 10 digits";
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Minimum 6 characters required";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    setErrors({
+      ...errors,
+      [e.target.name]:""
+    })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      localStorage.setItem('authData',JSON.stringify(formData))
+      alert("registration successfull")
+      navigate("/login")
+    }
+  };
+
   return (
     <div className="form-container">
-      {/*Page title */}
-      <h1 className="form-title ">Register</h1>
-      <form>
-        {/*Name field */}
+      <h1 className="form-title">Register</h1>
+
+      <form onSubmit={handleSubmit}>
+        {/* Name */}
         <div className="form-group">
-          <label htmlFor="name">Full Name</label>
+          <label>Full Name</label>
           <input
             type="text"
-            id="name"
             name="name"
-            placeholder="Enter your full name"
+            value={formData.name}
+            onChange={handleInputChange}
           />
+          {errors.name && <small className="error">{errors.name}</small>}
         </div>
-        {/*Email field */}
+
+        {/* Email */}
         <div className="form-group">
-          <label htmlFor="email">Email</label>
+          <label>Email</label>
           <input
             type="text"
-            id="email"
             name="email"
-            placeholder="Enter your email"
+            value={formData.email}
+            onChange={handleInputChange}
           />
+          {errors.email && <small className="error">{errors.email}</small>}
         </div>
-        {/*Phone number */}
+
+        {/* Phone */}
         <div className="form-group">
-          <label htmlFor="phone">Phone Number</label>
+          <label>Phone</label>
           <input
             type="tel"
-            id="phone"
             name="phone"
-            placeholder="Enter your phone number"
+            value={formData.phone}
+            onChange={handleInputChange}
           />
+          {errors.phone && <small className="error">{errors.phone}</small>}
         </div>
+
         {/* Password */}
         <div className="form-group">
-          <label htmlFor="password">Password</label>
+          <label>Password</label>
           <input
             type="password"
-            id="password"
             name="password"
-            placeholder="Enter your password"
+            value={formData.password}
+            onChange={handleInputChange}
           />
+          {errors.password && (
+            <small className="error">{errors.password}</small>
+          )}
         </div>
-        {/* SUbmit button */}
+
         <button type="submit" className="btn-primary">
           Register
         </button>
       </form>
+
       <p className="link-text">
         Already have an account? <Link to="/login">Login Here</Link>
       </p>
